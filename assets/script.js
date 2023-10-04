3cf12455d6a067d85cb41bdcffcfca970a27e6b4
Julie Paulus <julie.paulus@outlook.com>
2023-10-04 09:18:58 +0200

    04/10/2023 : Ajout des eventlisteners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+prevButton.addEventListener("click", slidePrevious);
+nextButton.addEventListener("click", slideNext);
+
+
+
+
+//const slides = [
+//	{
+//		"image":"slide1.jpg",
+//		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+//	},
+//	{
+//		"image":"slide2.jpg",
+//		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+//	},
+//	{
+//		"image":"slide3.jpg",
+//		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+//	},
+//	{
+//		"image":"slide4.png",
+//		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+//	}
+//]
