1338b4c5501fdc96c5f294996c976cd8428cfc16
Julie Paulus <julie.paulus@outlook.com>
2023-10-05 14:01:16 +0200

    05/10/2021 : Réorganisation de l'architecture et des fichiers

---
+++ assets/scripts/functions.js
@@ -0,0 +1,8 @@
+// Ajout des EventListener sur les deux flèches
+prevButton.addEventListener("click", () => {
+    console.log("Le bouton Prev fonctionne")
+});
+nextButton.addEventListener("click", () => {
+    console.log("Le bouton Next fonctionne")
+});
+
---
+++ assets/scripts/globales.js
@@ -0,0 +1,23 @@
+// Tableau de déclaration des slides et leur description
+const slides = [
+	{ // Index 0
+		"image":"./assets/images/slideshow/slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{ // Index 1
+		"image":"./assets/images/slideshow/slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{ // Index 2
+		"image":"./assets/images/slideshow/slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{ // Index 3
+		"image":"./assets/images/slideshow/slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+// Déclaration des variables :
+const prevButton = document.getElementById("Prev") // Flèche gauche
+const nextButton = document.getElementById("Next") // Flèche droite
